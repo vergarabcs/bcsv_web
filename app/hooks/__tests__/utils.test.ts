5d7f275e9031e620f1c3ec05dcb6a124bc5dfe99
vergarabcs-asurion <151128651+vergarabcs-asurion@users.noreply.github.com>
2024-04-23 13:40:27 +0800

    getHighlighted function

--- app/hooks/__tests__/utils.test.ts
+++ app/hooks/__tests__/utils.test.ts
@@ -1,5 +1,5 @@
 import { expect, test, describe } from '@jest/globals';
-import { DISTRIBUTION, TRIE, generateBoard } from "../utils";
+import { DISTRIBUTION, TRIE, generateBoard, getHighlighted, squarify } from "../utils";
 
 describe("utils", () => {
     test("Board should have correct count", () => {
@@ -30,5 +30,31 @@ describe("utils", () => {
     test('globalTrie should function as expected', () => {
         expect(TRIE.hasPurePrefix('car')).toBeTruthy()
         expect(TRIE.has('cara')).toBeTruthy()
+    })
+
+    test('getHighlighted returns correct indices', () => {
+        const board = [
+            ['C', 'G', 'N', 'Y', 'D'],
+            ['A', 'O', 'R', 'V', 'R'],
+            ['X', 'C', 'Z', 'D', 'Z'],
+            ['T', 'E', 'Q', 'N', 'U'],
+            ['N', 'L', 'Z', 'O', 'A']
+        ]
+        expect(
+            getHighlighted('CELN', board)
+        ).toEqual(
+            [ [ 2, 1 ], [ 3, 1 ], [ 4, 1 ], [ 4, 0 ] ]
+        )
+        expect(
+            getHighlighted('ZDNO', board)
+        ).not.toEqual(
+            [ [ 2, 1 ], [ 3, 1 ], [ 4, 1 ], [ 4, 2 ] ]
+        )
+        
+        const diagonals = ['QC', 'QL', 'QD', 'QO']
+        
+        diagonals.forEach((word:string) => {
+            expect(getHighlighted(word, board)).toEqual([])
+        })
     })
 })
--- app/hooks/utils.ts
+++ app/hooks/utils.ts
@@ -31,6 +31,13 @@ export const DISTRIBUTION = {
   Z: 1
 }
 
+const DIRECTION_OFFSET = [
+  [1, 0],
+  [0, 1],
+  [-1, 0],
+  [0, -1]
+]
+
 export const generateBoard = () => {
   const charSource: string[] = Object.entries(DISTRIBUTION).map(
     ([char, count]) => Array(count).fill(char)
@@ -40,8 +47,65 @@ export const generateBoard = () => {
   return Array(25).fill('').map(getRandomChar)
 }
 
-export const findWord = (word: string, board: string[][]) => {
+export const squarify = (dim: number, charList: string[]) => {
+  const newBoard: string[][] = []
+  charList.forEach((char, index) => {
+    if(index % dim === 0){
+      newBoard.push([])
+    }
+    newBoard[newBoard.length - 1].push(char)
+  })
+  return newBoard
+}
 
+export const getHighlighted = (word: string, board: string[][]) => {
+  const indices: number[][] = []
+  const isVisited: boolean[][] = board.map((row):boolean[] => row.map((_cell) => false))
+
+  const isValidIndex = (newX:number, newY:number) => {
+    return (
+      newX >= 0 &&
+      newX < board.length &&
+      newY >= 0 &&
+      newY < board[0].length
+    )
+  }
+
+  const dfs = (currX: number, currY: number): boolean => {
+    if(
+      !isValidIndex(currX, currY) ||
+      indices.length >= word.length ||
+      word.charAt(indices.length) !== board[currX][currY] ||
+      isVisited[currX][currY]
+    ){
+      return false
+    }
+    isVisited[currX][currY] = true
+    indices.push([currX, currY])
+    
+    if(
+      indices.length === word.length &&
+      indices.map((gridIndex) => board[gridIndex[0]][gridIndex[1]]).join('') === word
+    ){
+      return true
+    }
+    
+    for(const offset of DIRECTION_OFFSET){
+      if(dfs(currX + offset[0], currY + offset[1])){
+        return true
+      }
+    }
+
+    isVisited[currX][currY] = false
+    indices.pop()
+    return false
+  }
+  board.forEach((row, i) => row.forEach((_cell, j) => {
+    if(dfs(i, j)){
+      return indices
+    }
+  }))
+  return indices
 }
 
 // TODO: serializable prefix trie for smaller size
